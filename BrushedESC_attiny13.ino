31e60272b73937a4141f6a9898700142ffce8358
TheHLab <>
2019-10-20 22:05:43 +0700

    Add Code for ATtiny13

--- BrushedESC_attiny13.ino
+++ BrushedESC_attiny13.ino
@@ -1,47 +1,55 @@
 /*
 * Brushed ESC
 * @author TheHLab
-* @created Otc 10, 2019
-* This code for ATtiny85 with Arduino IDE, 8Mhz
+* @created Otc 18, 2019
+* This code for ATtiny13 with Arduino IDE - MCUdude/MicroCore, 9.6Mhz
 * Demo: https://youtu.be/hYDtW29BTBY
 */
+
+#include <PinChangeInterrupt.h>
 
 #define MIDDLE_POINT 1500
 #define OUT1 0
 #define OUT2 1
-#define IN 0
+#define IN 2
 
+volatile int pre_pwm_value = 0;
 volatile int pwm_value = 0;
 volatile int prev_time = 0;
  
 void setup() {
+  pinMode(OUT2, OUTPUT);
+  pinMode(OUT1, OUTPUT);
   // when pin IN goes high, call the rising function
-  attachInterrupt(IN, rising, RISING);
+  attachPCINT(IN, rising,RISING);
 }
  
-void loop() { }
+void loop() {}
  
 void rising() {
-  attachInterrupt(IN, falling, FALLING);
+  attachPCINT(IN, falling,FALLING);
   prev_time = micros();
 }
  
 void falling() {
-  attachInterrupt(IN, rising, RISING);
+  attachPCINT(IN, rising,RISING);
+  pre_pwm_value = pwm_value;
   pwm_value = micros()-prev_time;
+  if( pwm_value < 1000 || pwm_value > 2000 ){
+    pwm_value = pre_pwm_value;
+  }
   PWM_out(pwm_value);
 }
 
 void PWM_out(int speeds){
   // speed
   int tempSpeeds = map(abs(speeds-MIDDLE_POINT), 0, 500, 0, 255);
+  if(tempSpeeds>255) tempSpeeds = 255;
   // direction
   if( speeds > MIDDLE_POINT ){
     analogWrite(OUT1, tempSpeeds );
-    pinMode(OUT2, OUTPUT);
     digitalWrite(OUT2, LOW);
   } else {
-    pinMode(OUT1, OUTPUT);
     digitalWrite(OUT1, LOW);
     analogWrite(OUT2, tempSpeeds );
   }
